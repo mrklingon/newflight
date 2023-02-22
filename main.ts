23958142f03e28becc5cfef327323fa0af0fd711
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-02-22 14:43:07 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function mkLandscape (size: number) {
     land = []
     for (let index = 0; index < size; index++) {
-        land.push(randint(0, 2))
+        land.push(randint(-2, 2))
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -23,19 +23,45 @@ function showLand () {
         led.unplot(index2, 2)
         led.unplot(index2, 3)
         led.unplot(index2, 4)
-        led.plotBrightness(index2, 4 - land[index2], 88)
+        rock = 88
+        if (0 > land[index2]) {
+            rock = 200
+        }
+        led.plotBrightness(index2, 4 - Math.abs(land[index2]), rock)
     }
     land.push(land.shift())
 }
+let rock = 0
 let shipy = 0
 let shipx = 0
 let land: number[] = []
 let speed = 0
+images.createBigImage(`
+    # # # . . . . . . #
+    # . . # . # . # . #
+    # # . # . . # . . #
+    # . . # . . # . . .
+    # . . # # . # . . #
+    `).scrollImage(1, 200)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 mkLandscape(200)
 showLand()
-game.setLife(10)
+game.setLife(5)
 basic.forever(function () {
     showLand()
     basic.pause(200)
+    if (88 == led.pointBrightness(shipx, shipy)) {
+        game.removeLife(1)
+    }
+    if (200 == led.pointBrightness(shipx, shipy)) {
+        game.addLife(1)
+        game.addScore(randint(1, 10))
+    }
     led.plot(shipx, shipy)
 })
